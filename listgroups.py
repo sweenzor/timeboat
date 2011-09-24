4f13f315001fdc1d0e7ea331eb61e1ba9fc07e17
Matt Sweeney <mattswe@gmail.com>
2011-09-24 16:58:50 -0700

    Reformate payload

--- listgroups.py
+++ listgroups.py
@@ -15,21 +15,21 @@ config.read('key.cfg')
 #  ?client_id=YOUR_CLIENT_ID
 #  &token=YOUR_ACCESS_TOKEN
 
-payload = {
-  "group" : {
-    "topic"       : "Night Out",
-    "memberships" : [
+payload ={
+  'group' : {
+    'topic'       : "Night Out",
+    'memberships' : [
       {
-        "name"          : "Bob",
-        "phone_number"  : "+1 2125555555"
+        'name'          : "Bob",
+        'phone_number'  : "+1 2125555555"
       },
       {
-        "name"          : "Anne",
-        "email"         : "anne@example.com"
+        'name'          : "Anne",
+        'email'         : "anne@example.com"
       },
       {
-        "name"          : "John",
-        "user_id"       : "1234567892"
+        'name'          : "John",
+        'user_id'       : "1234567892"
       }
     ]
   }
@@ -37,14 +37,15 @@ payload = {
 
 
 http = httplib2.Http()
-
-url = 'https://api.groupme.com/clients/groups'
+url = 'https://api.groupme.com/clients/groups?'
+#url = 'https://api.groupme.com/clients/groups/1434247/memberships?'
 body = dict(client_id = config.get('groupme', 'client_id', 0),
+			client_secret = config.get('groupme', 'client_secret', 0),
 			token = config.get('groupme', 'token', 0))
 
-#print urllib.urlencode(body)+str(payload)
+print urllib.urlencode(body)+str(payload)
 print urllib.urlencode(body)
-response, content = http.request(url, 'GET', body=urllib.urlencode(body))
+response, content = http.request(url+urllib.urlencode(body)+str(payload), 'GET')
 
 print content
 print response
