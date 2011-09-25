0336989fa8e856600361f5e650c8f98e2fa6bb53
Matt Sweeney <mattswe@gmail.com>
2011-09-24 17:51:23 -0700

    Load config file a single time

--- groupme.py
+++ groupme.py
@@ -1,5 +1,6 @@
 import httplib2
 import urllib
+import json
 
 import ConfigParser
 
@@ -23,25 +24,33 @@ class groupme_auth(object):
 
 	def __init__(self):
 		self.http = httplib2.Http()
-		self.config = ConfigParser.ConfigParser()
-		self.config.read('keys.cfg')
+		config = ConfigParser.ConfigParser()
+		config.read('keys.cfg')
+		key_list = config.items('groupme')
+		self.keys = {}
+		for key in key_list:
+			self.keys[key[0]] = key[1]
 
 	def test_oauth(self):
 		pass
 
-
 	def request_token(self):
 
-		keys = dict(client_id = self.config.get('groupme', 'client_id', 0),
-					client_secret = self.config.get('groupme', 'client_secret', 0),
-					device_id = self.config.get('groupme', 'device_id', 0),
-					phone_number = self.config.get('groupme', 'phone_number', 0),
+		payload = dict(
+					client_id = self.keys['client_id'],
+					client_secret = self.keys['client_secret'],
+					device_id = self.keys['device_id'],
+					phone_number = self.keys['phone_number'],
 					grant_type = 'authorization_code',
-					code = self.config.get('groupme', 'code', 0))
+					code = self.keys['code'])
 		
-		payload = urllib.urlencode(keys)
-		resp, content = self.http.request("https://api.groupme.com/clients/tokens", "POST", payload)
-		return content
+		payload = urllib.urlencode(payload)
+		url = 'https://api.groupme.com/clients/tokens'
+		resp, content = self.http.request(url, 'POST', payload)
+		content = json.loads(content)
+		self.token = content[u'response'][u'access_token']
+
+		return self.token
 
 if __name__ == "__main__":
 
